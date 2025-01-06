a8f8e6985191b6d28687bb6df0e3a0f4e6fa95c3
sfishe10 <sfishe10@calpoly.edu>
2025-01-06 15:24:48 -0800

    first commit

---
+++ lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.zybooks.graduationrsvp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GraduationRSVPTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GradScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GradScreen(modifier: Modifier = Modifier) {
+    Text(
+        text = "Graduation Announcement",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GradPreview() {
+    GraduationRSVPTheme {
+        GradScreen()
+    }
+}
