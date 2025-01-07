8b25154e0bd2532824e5241060171b5b641eebb5
sfishe10 <sfishe10@calpoly.edu>
2025-01-06 16:20:18 -0800

    changed launcher icon

--- lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.zybooks.graduationrsvp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +34,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
     Text(
-        text = "Graduation Announcement",
+        text = stringResource(R.string.graduation_announcement),
+        fontSize = 50.sp,
         modifier = modifier
     )
 }
