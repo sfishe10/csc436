fbfc17863fbcf81d4a400dd0136872e6f824c445
sfishe10 <sfishe10@calpoly.edu>
2025-02-19 12:42:50 -0800

    added progress and completion notifications

--- lab4/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
+++ lab4/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -20,8 +20,20 @@ import androidx.work.workDataOf
 import com.zybooks.countdowntimer.ui.TimerScreen
 import com.zybooks.countdowntimer.ui.TimerViewModel
 import com.zybooks.countdowntimer.ui.theme.CountdownTimerTheme
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 
 class MainActivity : ComponentActivity() {
+
+   private val permissionRequestLauncher =
+      registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+         val message = if (isGranted) "Permission granted" else "Permission NOT granted"
+         Log.i("MainActivity", message)
+      }
 
    private val timerViewModel = TimerViewModel()
 
@@ -38,6 +50,13 @@ class MainActivity : ComponentActivity() {
             }
          }
       }
+      // Only need permission to post notifications on Tiramisu and above
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+         if (ActivityCompat.checkSelfPermission(this,
+               Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            permissionRequestLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+         }
+      }
    }
 
    override fun onStop() {
@@ -45,7 +64,14 @@ class MainActivity : ComponentActivity() {
 
       // Start TimerWorker if the timer is running
       if (timerViewModel.isRunning) {
-         startWorker(timerViewModel.remainingMillis)
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,
+                  Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+               startWorker(timerViewModel.remainingMillis)
+            }
+         } else {
+            startWorker(timerViewModel.remainingMillis)
+         }
       }
    }
 
